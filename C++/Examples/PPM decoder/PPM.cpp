09eecd8dc00d1efed26f916a9094b26b889f4998
Mikhail Avkhimenia <mikhail.avkhimenia@emlid.com>
2014-11-12 18:34:37 +0300

    Fixed PPM decoder

--- C++/Examples/PPM decoder/PPM.cpp
+++ C++/Examples/PPM decoder/PPM.cpp
@@ -23,7 +23,7 @@ unsigned int samplingRate      = 1;      // 1 microsecond (can be 1,2,4,5,10)
 unsigned int ppmInputGpio      = 4;      // PPM input on Navio's 2.54 header
 unsigned int ppmSyncLength     = 4000;   // Length of PPM sync pause
 unsigned int ppmChannelsNumber = 8;      // Number of channels packed in PPM
-undigned int servoFrequency    = 50;     // Servo control frequency
+unsigned int servoFrequency    = 50;     // Servo control frequency
 bool verboseOutputEnabled      = true;   // Output channels values to console
 
 //============================ Objects & data ==================================
