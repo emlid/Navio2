07a5d9faa681c290e04950e447b2b347ba81838d
Alexey Bulatov <alexey.bulatov@emlid.com>
2015-10-15 10:31:51 +0000

    Reworked example LED.cpp
    Added delay between changing color
    Added messages about color
    Changed channels for Red and Blue (now R=2, B=0), before
    was error

--- C++/Examples/LED/LED.cpp
+++ C++/Examples/LED/LED.cpp
@@ -33,33 +33,48 @@ int main()
     }
 
     PCA9685 pwm;
-    
+
     pwm.initialize();
 
     uint16_t R = 0, G = 0, B = 4095;
 
-    pwm.setPWM(0, R);
+    pwm.setPWM(2, R);
     pwm.setPWM(1, G);
-    pwm.setPWM(2, B);
+    pwm.setPWM(0, B);
+    printf("LED is yellow\n");
+    sleep(1);
 
     while (true) {
         for (R = 0; R < 4095; R++)
-            pwm.setPWM(0, R);
+            pwm.setPWM(2, R);
+        printf("LED is green\n");
+        sleep(1);
 
         for (B = 4095; B > 0; B--)
-            pwm.setPWM(2, B);
+            pwm.setPWM(0, B);
+        printf("LED is cyan\n");
+        sleep(1);
 
         for (G = 0; G < 4095; G++)
             pwm.setPWM(1, G);
+        printf("LED is blue\n");
+        sleep(1);
 
         for (R = 4095; R > 0; R--)
-            pwm.setPWM(0, R);
+            pwm.setPWM(2, R);
+        printf("LED is magneta\n");
+        sleep(1);
 
         for (B = 0; B < 4095; B++)
-            pwm.setPWM(2, B);
+            pwm.setPWM(0, B);
+        printf("LED is red\n");
+        sleep(1);
 
         for (G = 4095; G > 0; G--)
             pwm.setPWM(1, G);
+        printf("LED is yellow\n");
+        sleep(1);
+
     }
 
     return 0;
