8ea15ea71ad4c941233529975c2a9c65d4b5de23
Alexey Bulatov <alexey.bulatov@emlid.com>
2015-12-04 10:49:39 +0300

    Reworked example AccelGyroMag using InertialSensor interface
    Now example AccelGyroMag able to use 2 inertial sensors: MPU9250 and
    LSM9DS1. Implemented via abstract class InertialSensor.
    List of changes:
      -Added parsing of input parameters for choice of sensor.
      -Created function for allocation selected sensor.
      -Added information messages for help.
      -Rewrited comments to the file about using example.
      -Added to Makefile target for compilation "Navio/LSM9DS1.cpp".

--- C++/Examples/AccelGyroMag/AccelGyroMag.cpp
+++ C++/Examples/AccelGyroMag/AccelGyroMag.cpp
@@ -1,38 +1,103 @@
 /*
-Provided to you by Emlid Ltd (c) 2014.
+Provided to you by Emlid Ltd (c) 2015.
 twitter.com/emlidtech || www.emlid.com || info@emlid.com
 
 Example: Read accelerometer, gyroscope and magnetometer values from
-MPU9250 inertial measurement unit over SPI on Raspberry Pi + Navio.
+inertial measurement unit: MPU9250 or LSM9DS1 over SPI on Raspberry Pi + Navio.
 
-Navio's onboard MPU9250 is connected to the SPI bus on Raspberry Pi
-and can be read through /dev/spidev0.1
+Navio's onboard sensors are connected to the SPI bus on Raspberry Pi
+and can be read through /dev/spidev0.1 (MPU9250), /dev/spidev0.3 (acc/gyro LSM9DS1)
+and /dev/spidev0.2 (mag LSM9DS1).
 
 To run this example navigate to the directory containing it and run following commands:
 make
-./AccelGyroMag
+./AccelGyroMag -i [sensor name]
+Sensors names: mpu is MPU9250, lsm is LSM9DS1.
+For print help:
+./AccelGyroMag -h
 */
 
 #include "Navio/MPU9250.h"
+#include "Navio/LSM9DS1.h"
+#include <unistd.h>
+
+InertialSensor* create_inertial_sensor(char *sensor_name)
+{
+    InertialSensor *imu;
+
+    if (!strcmp(sensor_name, "mpu")) {
+        printf("Selected: MPU9250\n");
+        imu = new MPU9250();
+    }
+    else if (!strcmp(sensor_name, "lsm")) {
+        printf("Selected: LSM9DS1\n");
+        imu = new LSM9DS1();
+    }
+    else {
+        return NULL;
+    }
+
+    return imu;
+}
+
+void print_help()
+{
+    printf("Possible parameters:\nSensor selection: -i [sensor name]\n");
+    printf("Sensors names: mpu is MPU9250, lsm is LSM9DS1\nFor help: -h\n");
+}
 
 //=============================================================================
-
-int main()
+int main(int argc, char *argv[])
 {
-	//-------------------------------------------------------------------------
+    int parameter;
+    char *sensor_name;
 
-	MPU9250 imu;
-	imu.initialize();
+    if (argc < 2) {
+        printf("Enter parameter\n");
+        print_help();
+        return EXIT_FAILURE;
+    }
 
-	float ax, ay, az, gx, gy, gz, mx, my, mz;
+    // prevent the error message
+    opterr = 0;
 
-    //-------------------------------------------------------------------------
+    while ((parameter = getopt(argc, argv, "i:h")) != -1) {
+        switch (parameter) {
+        case 'i': sensor_name = optarg; break;
+        case 'h': print_help(); return EXIT_FAILURE;
+        case '?': printf("Wrong parameter.\n");
+                  print_help();
+                  return EXIT_FAILURE;
+        }
+    }
+
+    InertialSensor *sensor = create_inertial_sensor(sensor_name);
+
+    if (!sensor) {
+        printf("Wrong sensor name. Select: mpu or lsm\n");
+        return EXIT_FAILURE;
+    }
+
+    if (!sensor->probe()) {
+        printf("Sensor not enabled\n");
+        return EXIT_FAILURE;
+    }
+    sensor->initialize();
+
+    float ax, ay, az;
+    float gx, gy, gz;
+    float mx, my, mz;
+//-------------------------------------------------------------------------
 
     while(1) {
-        imu.getMotion9(&ax, &ay, &az, &gx, &gy, &gz, &mx, &my, &mz);
-	printf("Acc: %+7.3f %+7.3f %+7.3f  ", ax, ay, az);
-	printf("Gyr: %+8.3f %+8.3f %+8.3f  ", gx, gy, gz);
-	printf("Mag: %+7.3f %+7.3f %+7.3f\n", mx, my, mz);
-	
-	usleep(500000);
-    }}
+        sensor->update();
+        sensor->read_accelerometer(&ax, &ay, &az);
+        sensor->read_gyroscope(&gx, &gy, &gz);
+        sensor->read_magnetometer(&mx, &my, &mz);
+        printf("Acc: %+7.3f %+7.3f %+7.3f  ", ax, ay, az);
+        printf("Gyr: %+8.3f %+8.3f %+8.3f  ", gx, gy, gz);
+        printf("Mag: %+7.3f %+7.3f %+7.3f\n", mx, my, mz);
+
+       usleep(500000);
+    }
+}
