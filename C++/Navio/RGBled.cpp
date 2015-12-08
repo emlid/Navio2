0eb411a861700f453c73d9694fce037e708bfedb
Alexey Bulatov <alexey.bulatov@emlid.com>
2015-12-08 18:00:42 +0300

    Created RGBled driver
    It has 2 methods: initialize for tuning gpios, and setColor
    for set required color of LED.
    For choice of color using enum class Colors.

---
+++ C++/Navio/RGBled.cpp
@@ -0,0 +1,46 @@
+#include "RGBled.h"
+
+// Output is inverted
+#define OFF 1
+#define ON 0
+
+using namespace Navio;
+
+RGBled::RGBled()
+{
+}
+
+bool RGBled::initialize() {
+    pinR = new Pin(RPI_GPIO_4);
+    pinG = new Pin(RPI_GPIO_27);
+    pinB = new Pin(RPI_GPIO_6);
+    if (pinR->init() & pinG->init() & pinB->init()) {
+        pinR->setMode(Pin::GpioModeOutput);
+        pinG->setMode(Pin::GpioModeOutput);
+        pinB->setMode(Pin::GpioModeOutput);
+
+        // Switch of LED
+        pinR->write(OFF);
+        pinG->write(OFF);
+        pinB->write(OFF);
+    }
+    else {
+        fprintf(stderr, "Output enable not set. Are you root?\n");
+        return false;
+    }
+    return true;
+}
+
+void RGBled::setColor(Colors color)
+{
+    switch (color) {
+    case Colors::Black: pinR->write(OFF); pinG->write(OFF); pinB->write(OFF); break;
+    case Colors::Red: pinR->write(ON); pinG->write(OFF); pinB->write(OFF); break;
+    case Colors::Green: pinR->write(OFF); pinG->write(ON); pinB->write(OFF); break;
+    case Colors::Blue: pinR->write(OFF); pinG->write(OFF); pinB->write(ON); break;
+    case Colors::Cyan: pinR->write(OFF); pinG->write(ON); pinB->write(ON); break;
+    case Colors::Magneta: pinR->write(ON); pinG->write(OFF); pinB->write(ON); break;
+    case Colors::Yellow: pinR->write(ON); pinG->write(ON); pinB->write(OFF); break;
+    case Colors::White: pinR->write(ON); pinG->write(ON); pinB->write(ON); break;
+    }
+}
---
+++ C++/Navio/RGBled.h
@@ -0,0 +1,33 @@
+#ifndef _RGBLED_H_
+#define _RGBLED_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <cstdint>
+#include "gpio.h"
+
+enum class Colors {
+    Black,
+    Red,
+    Green,
+    Blue,
+    Cyan,
+    Magneta,
+    Yellow,
+    White};
+
+class RGBled {
+public:
+    RGBled();
+
+    bool initialize();
+    void setColor(Colors color);
+
+private:
+    Navio::Pin *pinR;
+    Navio::Pin *pinG;
+    Navio::Pin *pinB;
+};
+
+#endif //_RGBLED_H_
