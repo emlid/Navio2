665f703ec6df0ae9a170f2daeb3b2eda45da7d5e
Staroselskii Georgii <gosha371@gmail.com>
2016-02-05 15:35:08 +0300

    adc: adadpted to Navio2

--- C++/Examples/ADC/ADC.cpp
+++ C++/Examples/ADC/ADC.cpp
@@ -12,34 +12,26 @@ make
 ./ADC
 */
 
-#include <stdio.h>
 #include <unistd.h>
+#include <err.h>
+#include <cstdint>
+#include <cstdio>
 
-#include "Navio/ADS1115.h"
+#include <Navio/Util.h>
 
-#define ARRAY_SIZE(array) (sizeof(array)/sizeof(array[0]))
 int main() {
 
-    ADS1115 adc;
-
-    adc.setMode(ADS1115_MODE_SINGLESHOT);
-    adc.setRate(ADS1115_RATE_860); 
-
-    uint16_t muxes[] = {ADS1115_MUX_P0_NG, ADS1115_MUX_P1_NG, ADS1115_MUX_P2_NG, ADS1115_MUX_P3_NG};
-    float results[ARRAY_SIZE(muxes)] = {0.0f};
-    int i = 0;
+    const char *channel_path = "/sys/kernel/rcio/adc/ch0";
 
     while (true) {
-        adc.setMultiplexer(muxes[i]);
+        uint32_t conversion;
 
-        float conversion = adc.getMilliVolts();
-        results[i] = conversion;
-
-        i = (i + 1) % ARRAY_SIZE(muxes);
-
-        for (int j = 0; j < ARRAY_SIZE(muxes); j++) {
-            printf("A%d: %.4fV ", j, results[j] / 1000);
+        if (read_file(channel_path, "%u", &conversion) < 0) {
+            warn("Unable to get voltage");
         }
+
+        printf("%.4fV ", static_cast<float> (conversion / 1000.0f));
+
         printf("\n");
     }
 
