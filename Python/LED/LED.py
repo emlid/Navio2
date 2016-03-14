5f2550ebcabf56f1a570cddee136ca83b8d2a111
Ivan Smirnov <ivan.smirnov@emlid.com>
2016-03-14 09:47:31 +0000

    python: LED: adapted to Navio2
    LED now controlled via /sys/class/leds.

--- Python/LED/LED.py
+++ Python/LED/LED.py
@@ -1,78 +1,38 @@
-"""
-Provided to you by Emlid Ltd (c) 2014.
-twitter.com/emlidtech || www.emlid.com || info@emlid.com
-
-Example: Control the RGB LED onboard of Navio shield for Raspberry Pi
-
-Please note that this example uses Adafruit I2C and PCA9685 drivers.
-
-To run this example navigate to the directory containing it and run following commands:
-sudo python LED.py
-"""
-
-from Adafruit_PWM_Servo_Driver import PWM
-import time
-
 import sys
 
 sys.path.append('..')
 
-from Navio import GPIO
+from Navio import LEDS
+import time
 
-#drive Output Enable in PCA low
-pin = GPIO.Pin(27)
-pin.write(0)
+led = LEDS.Led()
 
-pwm = PWM(0x40, debug=False)
-
-# Set frequency to 60 Hz
-pwm.setPWMFreq(60)
-
-step = 1 # light color changing step
-
-# set initial color
-R = 0
-G = 0
-B = 4095
-pwm.setPWM(0, 0, B)
+led.setColor('Yellow')
 print "LED is yellow"
 time.sleep(1)
 
 while (True):
-    for R in xrange(0, 4095, step):
-        pwm.setPWM(2, 0, R)
+
+    led.setColor('Green')
     print "LED is green"
     time.sleep(1)
 
-    for B in xrange(4095, 0, -step):
-        pwm.setPWM(0, 0, B)
+    led.setColor('Cyan')
     print "LED is cyan"
     time.sleep(1)
-    
-    for G in xrange(0, 4095, step):
-        pwm.setPWM(1, 0, G)
+
+    led.setColor('Blue')
     print "LED is blue"
     time.sleep(1)
 
-    for R in xrange(4095, 0, -step):
-        pwm.setPWM(2, 0, R)
+    led.setColor('Magneta')
     print "LED is magneta"
     time.sleep(1)
 
-    for B in xrange(0, 4095, step):
-        pwm.setPWM(0, 0, B)
+    led.setColor('Red')
     print "LED is red"
     time.sleep(1)
 
-    for G in xrange(4095, 0, -step):
-        pwm.setPWM(1, 0, G)
+    led.setColor('Yellow')
     print "LED is yellow"
     time.sleep(1)
-
-  # Change speed of continuous servo on channel O
-  # pwm.setPWM(0, 0, servoMin)
-  # time.sleep(1)
-  # pwm.setPWM(0, 0, servoMax)
-  # time.sleep(1)
-
-
