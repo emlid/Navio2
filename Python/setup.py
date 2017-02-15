c959e6595d17e6adfcd413dd03f165a01ae03cb0
Georgii Staroselskii <gosha371@gmail.com>
2017-02-15 15:06:51 +0000

    python: package: add initial package

---
+++ Python/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="navio2",
+    version="0.1.0",
+    license="GPL",
+    author="Georgii Staroselskii",
+    author_email="georgii.staroselskii@emlid.com",
+    url="https://github.com/emlid/navio2",
+    packages=['navio2']
+)
