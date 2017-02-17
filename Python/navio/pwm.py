a2675a7a60baf441dde766ced521d182da4e2bef
Georgii Staroselskii <gosha371@gmail.com>
2017-02-17 09:35:21 +0000

    python: pwm: add context manager mixin

--- Python/navio/pwm.py
+++ Python/navio/pwm.py
@@ -3,13 +3,24 @@ import os.path
 class PWM():
     SYSFS_PWM_PATH_BASE = "/sys/class/pwm/pwmchip0/"
     SYSFS_PWM_EXPORT_PATH = "/sys/class/pwm/pwmchip0/export"
+    SYSFS_PWM_UNEXPORT_PATH = "/sys/class/pwm/pwmchip0/unexport"
 
     def __init__(self, channel):
         self.channel = channel
         self.channel_path = self.SYSFS_PWM_PATH_BASE + "pwm{}/".format(self.channel)
+        self.is_initialized = False
 
+    def __enter__(self):
+        self.is_initialized = self.initialize()
+        return self
+
+    def __exit__(self, *args):
+        with open(self.SYSFS_PWM_UNEXPORT_PATH, "a") as pwm_unexport:
+            pwm_unexport.write(str(self.channel))
+
+    def initialize(self):
         if not os.path.exists(self.SYSFS_PWM_PATH_BASE):
-            raise IOError("rcio_pwm module wasn't loaded")
+            raise OSError("rcio_pwm module wasn't loaded")
 
         if not os.path.exists(self.channel_path):
             with open(self.SYSFS_PWM_EXPORT_PATH, "a") as pwm_export:
@@ -18,12 +29,21 @@ class PWM():
         with open(self.channel_path + "enable", "w") as pwm_enable:
             pwm_enable.write("1")
 
+        return True
+
+
     def set_period(self, freq):
+        if not self.is_initialized:
+            raise RuntimeError("ADC not initialized. Call initialize first")
+
         period_ns = int(1e9/freq)
         with open(self.channel_path + "period",  "w") as pwm_period:
             pwm_period.write(str(period_ns))
 
     def set_duty_cycle(self, period):
+        if not self.is_initialized:
+            raise RuntimeError("ADC not initialized. Call initialize first")
+
         period_ns = int(period*1e6)
         with open(self.channel_path + "duty_cycle", "w") as pwm_duty:
             pwm_duty.write(str(period_ns))
