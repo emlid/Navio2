1f2a19626899fdb35f199aed4ebaeb45a7e0bdf1
Georgii Staroselskii <gosha371@gmail.com>
2017-02-17 09:40:58 +0000

    python: servo: use newer context manager API
    This way there won't be any unexported PWM handles in
    /sys/class/pwm/pwmchip0

--- Python/Servo.py
+++ Python/Servo.py
@@ -10,11 +10,11 @@ PWM_OUTPUT = 0
 SERVO_MIN = 1.250 #ms
 SERVO_MAX = 1.750 #ms
 
-pwm = navio.pwm.PWM(PWM_OUTPUT)
-pwm.set_period(50)
+with navio.pwm.PWM(PWM_OUTPUT) as pwm:
+    pwm.set_period(50)
 
-while (True):
-    pwm.set_duty_cycle(SERVO_MIN)
-    time.sleep(1)
-    pwm.set_duty_cycle(SERVO_MAX)
-    time.sleep(1)
+    while (True):
+        pwm.set_duty_cycle(SERVO_MIN)
+        time.sleep(1)
+        pwm.set_duty_cycle(SERVO_MAX)
+        time.sleep(1)
