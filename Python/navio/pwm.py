162bf2f8bd0726e4f6221fa9be31d36179037a7e
Georgii Staroselskii <gosha371@gmail.com>
2017-02-17 09:01:20 +0000

    python: pwm: improve error handling

--- Python/navio/pwm.py
+++ Python/navio/pwm.py
@@ -1,23 +1,29 @@
+import os.path
+
 class PWM():
+    SYSFS_PWM_PATH_BASE = "/sys/class/pwm/pwmchip0/"
+    SYSFS_PWM_EXPORT_PATH = "/sys/class/pwm/pwmchip0/export"
 
     def __init__(self, channel):
         self.channel = channel
-        try:
-            with open("/sys/class/pwm/pwmchip0/export", "a") as pwm_export:
+        self.channel_path = self.SYSFS_PWM_PATH_BASE + "pwm{}/".format(self.channel)
+
+        if not os.path.exists(self.SYSFS_PWM_PATH_BASE):
+            raise IOError("rcio_pwm module wasn't loaded")
+
+        if not os.path.exists(self.channel_path):
+            with open(self.SYSFS_PWM_EXPORT_PATH, "a") as pwm_export:
                 pwm_export.write(str(self.channel))
-        except IOError:
-            #already exported. nothing to 
-            pass
-        
-        with open("/sys/class/pwm/pwmchip0/pwm%d/enable" % self.channel, "w") as pwm_enable:
+
+        with open(self.channel_path + "enable", "w") as pwm_enable:
             pwm_enable.write("1")
 
     def set_period(self, freq):
         period_ns = int(1e9/freq)
-        with open("/sys/class/pwm/pwmchip0/pwm%d/period" % self.channel, "w") as pwm_period:
+        with open(self.channel_path + "period",  "w") as pwm_period:
             pwm_period.write(str(period_ns))
 
     def set_duty_cycle(self, period):
         period_ns = int(period*1e6)
-        with open("/sys/class/pwm/pwmchip0/pwm%d/duty_cycle" % self.channel, "w") as pwm_duty:
+        with open(self.channel_path + "duty_cycle", "w") as pwm_duty:
             pwm_duty.write(str(period_ns))
