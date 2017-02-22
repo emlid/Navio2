513a4b8ab9cf67d263ff59737127549469cc39a0
Georgii Staroselskii <gosha371@gmail.com>
2017-02-22 15:24:54 +0000

    python: setup.py: version bump

--- Python/setup.py
+++ Python/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="navio2",
-    version="0.1.0",
+    version="0.1.1",
     license="BSD",
     description="Navio 2 device drivers",
     author="Georgii Staroselskii",
