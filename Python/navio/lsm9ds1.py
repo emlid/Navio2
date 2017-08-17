ad76a36b8847f98e63cb223c164976039d70a0b6
Guido Sanchez <sanchez.guido@gmail.com>
2017-08-17 14:13:01 -0300

    Update lsm9ds1.py

--- Python/navio/lsm9ds1.py
+++ Python/navio/lsm9ds1.py
@@ -1,6 +1,6 @@
 """
 LSM9DS1 driver code is placed under the BSD license.
-Copyright (c) 2014, Emlid Limited, www.emlid.com
+Copyright (c) 2017, Emlid Limited, www.emlid.com
 All rights reserved.
 Redistribution and use in source and binary forms, with or without
 modification, are permitted provided that the following conditions are met:
