e6b4e1fa020a412aba9c65f47e101813fc3b6921
Igor Anokhin <igor.anohin@emlid.com>
2017-10-20 16:31:00 +0300

    C++: Examples: ADC: add Navio+ support
    Makefile: add Navio+ libraries
    ADC.cpp: add Navio+ entry

--- C++/Examples/ADC/ADC.cpp
+++ C++/Examples/ADC/ADC.cpp
@@ -1,27 +1,51 @@
 #include <unistd.h>
 #include <cstdio>
-#include <Navio/Util.h>
-#include <Navio/ADC.h>
+#include <Common/Util.h>
+#include <Navio2/ADC_Navio2.h>
+#include <Navio+/ADC_Navio.h>
+#include <memory>
+
+#define READ_FAILED -1
+
+
+
+std::unique_ptr <ADC> get_converter()
+{
+    if (get_navio_version() == NAVIO2)
+    {
+        auto ptr = std::unique_ptr <ADC>{ new ADC_Navio2() };
+        return ptr;
+    } else
+    {
+        auto ptr = std::unique_ptr <ADC>{ new ADC_Navio() };
+        return ptr;
+    }
+
+}
+
 
 int main(int argc, char *argv[])
 {
-    ADC adc{};
-    adc.init();
-    float results[adc.get_channel_count()] = {0.0f};
-
     if (check_apm()) {
         return 1;
     }
-
-    while (true) {
-        for (int i = 0; i < ARRAY_SIZE(results); i++){
-            results[i] = adc.read(i);
+    auto adc = get_converter();
+    adc->initialize();
+    float results[adc->get_channel_count()] = {0.0f};
+    while (true)
+    {
+        for (int i = 0; i < adc->get_channel_count(); i++)
+        {
+            results[i] = adc->read(i);
+            if (results[i] == READ_FAILED)
+                return EXIT_FAILURE;
             printf("A%d: %.4fV ", i, results[i] / 1000);
         }
         printf("\n");
-        
+
         usleep(500000);
     }
+
 
     return 0;
 }
