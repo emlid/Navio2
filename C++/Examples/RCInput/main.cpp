afac1b68e7686df22fae10c471cc309b68c6580e
Igor Anokhin <igor.anohin@emlid.com>
2017-10-20 17:08:17 +0300

    C++: Examples: RCInput: add Navio+ support
    main: rewrite with RCInput interface
    Makefile: add new libraries from Navio+

--- C++/Examples/RCInput/main.cpp
+++ C++/Examples/RCInput/main.cpp
@@ -1,22 +1,43 @@
 #include <unistd.h>
 #include <cstdio>
 
-#include <Navio/RCInput.h>
-#include "Navio/Util.h"
+#include <Navio2/RCInput_Navio2.h>
+#include <Navio+/RCInput_Navio.h>
+#include <Common/Util.h>
+#include <memory>
+
+#define READ_FAILED -1
+
+std::unique_ptr <RCInput> get_rcin()
+{
+    if (get_navio_version() == NAVIO2)
+    {
+        auto ptr = std::unique_ptr <RCInput>{ new RCInput_Navio2() };
+        return ptr;
+    } else
+    {
+        auto ptr = std::unique_ptr <RCInput>{ new RCInput_Navio() };
+        return ptr;
+    }
+
+}
+
 
 int main(int argc, char *argv[])
 {
-    RCInput rcin{};
-
     if (check_apm()) {
         return 1;
     }
 
-    rcin.init();
+    auto rcin = get_rcin();
 
-    while (true) 
+    rcin->initialize();
+
+    while (true)
     {
-        int period = rcin.read(2);
+        int period = rcin->read(2);
+        if (period == READ_FAILED)
+            return EXIT_FAILURE;
         printf("%d\n", period);
         
         sleep(1);
@@ -24,3 +45,4 @@ int main(int argc, char *argv[])
 
     return 0;
 }
+
