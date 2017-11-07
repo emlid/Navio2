dfdeb5df07a7c3c5323c6976b1a23f91f48b5d44
Igor Anokhin <igor.anohin@emlid.com>
2017-11-07 17:25:41 +0300

    C++: Navio: Common: fix initialize bug
    Change PERI_BASE for raspberry 3

--- C++/Navio/Common/gpio.cpp
+++ C++/Navio/Common/gpio.cpp
@@ -18,7 +18,7 @@
 /* Raspberry Pi GPIO memory */
 #define BCM2708_PERI_BASE   0x20000000
 #define BCM2709_PERI_BASE   0x3F000000
-#define BCM2835_PERI_BASE   0x20000000
+#define BCM2835_PERI_BASE   0x3F000000
 #define GPIO_BASE(address)  (address + 0x200000)
 #define PAGE_SIZE           (4*1024)
 #define BLOCK_SIZE          (4*1024)
