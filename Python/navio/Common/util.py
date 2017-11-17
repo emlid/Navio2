6c0c9236f050d4db558cf25753aa2cb39b7b2bd0
Igor Anokhin <igor.anohin@emlid.com>
2017-11-17 17:57:36 +0300

    python: navio: Common: util: add new function
    Add function get_navio_version, which return Navio version

--- Python/navio/Common/util.py
+++ Python/navio/Common/util.py
@@ -5,3 +5,8 @@ def check_apm():
     ret = sub.call(["ps -AT | grep -c ap-timer > /dev/null"], shell = True)
     if ret <= 0:
         sys.exit("APM is running. Can't launch the example")
+
+def get_navio_version():
+    file = open("/sys/firmware/devicetree/base/hat/product_id")
+    version = "NAVIO" if file.readline(6) == "0x0001" else "NAVIO2"
+    return version
